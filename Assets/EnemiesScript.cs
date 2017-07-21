fa23eee3a37f5e32d26a39a475c30a93c1c3778a
arisgi <m1so.tama16@gmail.com>
2017-07-22 01:10:50 +0900

    Add enemies

---
+++ Assets/EnemiesScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesScript : MonoBehaviour {
+
+	public GameObject enemy;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.frameCount % 60 == 0) {
+			Instantiate(enemy, new Vector3(Random.Range(-5.0F, 5.0F), 1.0F, 8.0F), transform.rotation);
+		}
+	}
+}
---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.z -= 0.1F;
+		transform.position = pos;
+		transform.Rotate(1, 1, 1);
+	}
+
+	void OnCollisionEnter() {
+		Destroy(gameObject);
+	}
+}
