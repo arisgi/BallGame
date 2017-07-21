88154793a772dfe72f677f7bbcb52e007d19e919
arisgi <m1so.tama16@gmail.com>
2017-07-21 15:23:52 +0900

    Move the player according to key

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		float x = Input.GetAxis("Horizontal");
+		transform.Translate(x * 0.2F, 0, 0);
 	}
 }
